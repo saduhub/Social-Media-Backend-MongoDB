e483d0e5b067cf3012b6c9c5057b431f67c3d27c
Jorge Duarte <sadujorge@gmail.com>
2023-08-06 11:53:50 -0700

    Setup connection to mongodb, installed initial dependencies, and env variables.

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+// Require MongoClient class (Object needed for connection).
+const { MongoClient } = require('mongodb');
+const app = express();
+const port = 3001;
+// Connection address. 
+const connection = process.env.DB_CONNECTION;
+// New instance of connection sing MongoClient driver. 
+const client = new MongoClient(connection);
+// Will store connection to database.
+let db;
+// Database name
+const dbName = 'socialDB';
+// Connect method to connect to Mongo.
+client.connect()
+  .then(() => {
+    console.log('Mongo Connected');
+    // Make new database instance (drops?)
+    db = client.db(dbName);
+    // Start express
+    app.listen(port, () => {
+      console.log(`Listening at port ${port}`);
+    });
+  })
+    //Whichever connection fails
+  .catch((err) => {
+    console.error('Error: ', err.message);
+  });
+
+
+
