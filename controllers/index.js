3a002f88d1f1abe2ab9be4b7ec5eefadb7b4048e
Jorge Duarte <sadujorge@gmail.com>
2024-02-19 15:51:19 -0800

    Finished setting up router

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-
-// api index.js '/'
-router.use('/api/', apiRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,20 @@
 const express = require('express');
 const db = require('./config/config')
-const { User, Thought } = require('./models/index');
+const routes = require('./controllers');
 
 const app = express();
 const port = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(require('./controllers/'));
-// Finds all Thought documents
-app.get('/thoughts', async (req, res) => {
-    try {
-      const thoughts = await Thought.find({});
-      res.status(200).json(thoughts);
-    } catch (err) {
-      console.log(Error);
-      res.status(500).json({ message: err });
-    }
-  });
+app.use(routes);
 
 // Sync models and connect to db due to the db.once. Also ensures that server is listening only when the db is accessible. 
 db.once('open', () => {
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     });
-  });
+});
 
 
 
